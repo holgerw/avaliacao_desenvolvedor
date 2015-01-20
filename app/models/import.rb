8340ccfcb8a707b9f3154673dec1f0a709cba74c
Holger Araújo Wiehen <wiehen@gmail.com>
2015-01-20 13:27:21 -0200

    Parse uploaded file and normalize data

--- app/models/import.rb
+++ app/models/import.rb
@@ -8,9 +8,32 @@
 #  updated_at :datetime         not null
 #
 
-class Import < ActiveRecord::Base
+require 'csv'
 
-  # non persistent attribute
-  attr_accessor :file
+class Import < ActiveRecord::Base
+  # non persistent attributes
+  attr_accessor :file, :import_data
+  # validations
+  validates :saved_file, presence: true
+
+  def parse_file
+    return nil unless File.file?(saved_file)
+    self.import_data = CSV.read(saved_file, { :col_sep => "\t" })
+    header = import_data.shift
+    header.map! { |item| Import.normalize_string(item).to_sym }
+    import_data.map! do |row|
+      row_hash = {}
+      row.each_with_index do |item, index|
+        row_hash[ header[index] ] = item
+      end
+      row_hash
+    end
+  end
+
+  # auxiliar methods (public for testing purpose)
+  def self.normalize_string orig_string
+    ascii_string = ActiveSupport::Inflector.transliterate( orig_string )
+    ascii_string.downcase.gsub(' ', '_')
+  end
 
 end
